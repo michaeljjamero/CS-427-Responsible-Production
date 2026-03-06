8727489d0c9071f69090f1fd09bf43e888e4b7fc
Kristine-M <kma28@uic.edu>
2026-03-05 21:44:58 -0600

    changes

--- Assets/Scripts/TvKwhActivate.cs
+++ Assets/Scripts/TvKwhActivate.cs
@@ -4,43 +4,112 @@ using UnityEngine;
 
 public class TvKwhActivate : MonoBehaviour
 {
-    
-
-    //// Update is called once per frame
-    //void Update()
-    //{
-
-    //}
-    [Header("Assign the parent that holds all kWh objects (or a single object)")]
-    [SerializeField] private GameObject kwhGroup;
-    [SerializeField] private GameObject tvCanvas; // Drag Canvas here
 
 
+    ////// Update is called once per frame
+    ////void Update()
+    ////{
 
-    [Header("Optional: click settings")]
-    [SerializeField] private Camera cam; // leave empty to auto-use Camera.main
-    [SerializeField] private bool toggle = true; // true = click toggles on/off, false = only turns on
+    ////}
+    //[Header("Assign the parent that holds all kWh objects (or a single object)")]
+    //[SerializeField] private GameObject kwhGroup;
+    //[SerializeField] private GameObject tvCanvas; // Drag Canvas here
 
-    //private void Awake()
+
+
+    //[Header("Optional: click settings")]
+    //[SerializeField] private Camera cam; // leave empty to auto-use Camera.main
+    //[SerializeField] private bool toggle = true; // true = click toggles on/off, false = only turns on
+
+    ////private void Awake()
+    ////{
+    ////    if (cam == null) cam = Camera.main;
+
+    ////    // Hide on start
+    ////    if (kwhGroup != null)
+    ////        kwhGroup.SetActive(false);
+    ////    else
+    ////        Debug.LogError("TvKwhActivate: kwhGroup is not assigned.");
+    ////}
+
+    //// Start is called before the first frame update
+    //void Start()
     //{
     //    if (cam == null) cam = Camera.main;
 
-    //    // Hide on start
     //    if (kwhGroup != null)
     //        kwhGroup.SetActive(false);
     //    else
     //        Debug.LogError("TvKwhActivate: kwhGroup is not assigned.");
+
+    //    if (tvCanvas != null)
+    //        tvCanvas.SetActive(false); // TV starts blank
     //}
 
-    // Start is called before the first frame update
+    //private void Update()
+    //{
+    //    if (Input.GetMouseButtonDown(0)) // left click
+    //    {
+    //        if (cam == null) return;
+
+    //        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+    //        if (Physics.Raycast(ray, out RaycastHit hit))
+    //        {
+    //            // Did we click THIS TV (or one of its children)?
+    //            if (hit.transform == transform || hit.transform.IsChildOf(transform))
+    //            {
+    //                ActivateKwh();
+    //            }
+    //        }
+    //    }
+    //}
+
+    //private void ActivateKwh()
+    //{
+    //    if (kwhGroup == null) return;
+
+    //    if (toggle)
+    //        kwhGroup.SetActive(!kwhGroup.activeSelf);
+    //    else
+    //        kwhGroup.SetActive(true);
+
+    //    if (tvCanvas != null)
+    //    {
+    //        if (toggle)
+    //            tvCanvas.SetActive(!tvCanvas.activeSelf);
+    //        else
+    //            tvCanvas.SetActive(true);
+    //    }
+    //}
+
+    [Header("Assign the parent that holds all kWh objects")]
+    [SerializeField] private GameObject kwhGroup;
+
+    [SerializeField] private GameObject tvCanvas;
+
+    [Header("Optional: click settings")]
+    [SerializeField] private Camera cam;
+
+    private bool tvIsOn = false;
+    private KwhMovement bobScript;
+
     void Start()
     {
-        if (cam == null) cam = Camera.main;
+        if (cam == null)
+            cam = Camera.main;
 
         if (kwhGroup != null)
-            kwhGroup.SetActive(false);
+        {
+            kwhGroup.SetActive(false); 
+            bobScript = kwhGroup.GetComponent<KwhMovement>();
+
+            if (bobScript != null)
+                bobScript.StopFloating();
+        }
         else
+        {
             Debug.LogError("TvKwhActivate: kwhGroup is not assigned.");
+        }
 
         if (tvCanvas != null)
             tvCanvas.SetActive(false); // TV starts blank
@@ -48,37 +117,55 @@ public class TvKwhActivate : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0)) // left click
+        if (Input.GetMouseButtonDown(0))
         {
             if (cam == null) return;
 
             Ray ray = cam.ScreenPointToRay(Input.mousePosition);
             if (Physics.Raycast(ray, out RaycastHit hit))
             {
-                // Did we click THIS TV (or one of its children)?
                 if (hit.transform == transform || hit.transform.IsChildOf(transform))
                 {
-                    ActivateKwh();
+                    ToggleTV();
                 }
             }
         }
     }
 
-    private void ActivateKwh()
+    //private void ToggleTV()
+    //{
+    //    tvIsOn = !tvIsOn;
+
+    //    if (tvCanvas != null)
+    //        tvCanvas.SetActive(tvIsOn);
+
+    //    if (bobScript != null)
+    //    {
+    //        if (tvIsOn)
+    //            bobScript.StartFloating();
+    //        else
+    //            bobScript.StopFloating();
+    //    }
+    //}
+    private void ToggleTV()
     {
-        if (kwhGroup == null) return;
+        tvIsOn = !tvIsOn;
 
-        if (toggle)
-            kwhGroup.SetActive(!kwhGroup.activeSelf);
-        else
-            kwhGroup.SetActive(true);
-
+        // Show / hide the TV picture
         if (tvCanvas != null)
+            tvCanvas.SetActive(tvIsOn);
+
+        if (bobScript != null)
         {
-            if (toggle)
-                tvCanvas.SetActive(!tvCanvas.activeSelf);
+            if (tvIsOn)
+            {
+                kwhGroup.SetActive(true);   // SHOW kWh when TV turns on
+                bobScript.StartFloating();
+            }
             else
-                tvCanvas.SetActive(true);
+            {
+                bobScript.StopFloating();   // stop movement but keep visible
+            }
         }
     }
 }
